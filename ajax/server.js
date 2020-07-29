e5863c8e89a42b8d5dd2ab54ed2ebb3d3a0282c9
tadeutr <tadeu.tripoli@gmail.com>
2020-07-29 10:46:46 -0300

    29_07

---
+++ ajax/server.js
@@ -0,0 +1,42 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express()
+
+
+//middlewares
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({storage}).single('arquivo')
+
+app.post('./upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('ocorreu um erro')
+        }
+
+        res.end('concluido com sucesso')
+    })
+})
+
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+
+// app.get('/teste', (req, res) => res.send(new Date))
+app.listen(8080, () => console.log('Executando...'));
